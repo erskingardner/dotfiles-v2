4f66111940ba30cc99715e9d7b4c15b3b11d3229
Jeff Gardner <erskingardner@gmail.com>
2012-08-01 09:15:25 +0200

    adding test alias

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -14,12 +14,14 @@ alias bi="bundle install"
 alias bip="bundle install $BUNDLE_PATH"
 alias cuke='bundle exec cucumber'
 alias sp='bundle exec rspec'
+alias tests='bundle exec rspec && bundle exec cucumber'
 
 alias ff3k='open -a Firefox http://localhost:3000'
 alias ch3k='open -a "Google Chrome" http://localhost:3000'
 alias sa3k='open -a Safari http://localhost:3000'
 alias 3k='open http://localhost:3000' # use the default browser
 alias be="bundle exec"
+
 
 # Heroku
 alias hercons='heroku run console --remote'
