e29e6cfa695e041b11544de555aefdb5f6cc6366
Jeff Gardner <erskingardner@gmail.com>
2013-10-08 13:03:36 +0200

    Adding some aliases, also adding python to path.

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -20,7 +20,9 @@ alias be="bundle exec"
 alias schema_reset='RAILS_ENV=test bundle exec rake db:drop && RAILS_ENV=test bundle exec rake db:create && RAILS_ENV=test bundle exec rake db:migrate'
 
 alias z='zeus'
-alias mc='muster console'
+alias zt='zeus test'
+alias zc='zeus cucumber'
+alias mc='muster console production'
 
 # Heroku
 alias hercons='heroku run console --remote'
