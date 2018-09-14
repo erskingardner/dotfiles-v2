fddffc49d9490ca470b4a9a9020a6e3528697607
Jeff Gardner <erskingardner@gmail.com>
2018-09-14 09:15:54 +0200

    Cleaning up and adding a few more aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,20 +1,3 @@
-# Use `hub` as our git wrapper:
-#   http://defunkt.github.com/hub/
-#
-# I use JRuby substantially, and we want to make sure hub is run using MRI
-# regardless of which Ruby you're using or else the `git status` in your prompt
-# will take seven thousand seconds to run `ls`.
-#
-# I'm hardcoding it to an installed rvm (using rvm's `rvm 1.9.3,ruby /hub/path`
-# syntax is way too slow). It should work fine for those without rvm, though.
-# if [[ -s $HOME/.rvm/scripts/rvm ]]
-# then
-#   if $(which hub &> /dev/null) && [[ -s $HOME/.rvm/rubies/ruby-1.9.3-p0 ]]
-#   then
-#     alias git='$HOME/.rvm/rubies/ruby-1.9.3-p0/bin/ruby `which hub`'
-#   else
-#   fi
-# fi
 # Hub!
 alias git=hub
 
@@ -27,8 +10,8 @@ alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
 alias gb='git branch'
-alias gcb='git co -b'
-alias go='git co -'
+alias gcb='git checkout -b'
+alias go='git checkout -'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
 alias glg='git lg'
@@ -39,3 +22,4 @@ alias gbr='git browse'
 alias gbi='git browse -- issues'
 alias gx='gitx .'
 alias gaa='git add --all'
+alias gitclean='git branch --merged | egrep -v "(^\*|master|dev)" | xargs git branch -d'
--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,5 @@
-export PATH="/usr/local/share/python:/usr/local/share/npm/bin:/usr/local/bin:/user/local/mysql/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH="/usr/local/share/python:/usr/local/share/npm/bin:/usr/local/bin:/user/local/mysql/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$HOME/.pilot/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+eval $(pilot env)
